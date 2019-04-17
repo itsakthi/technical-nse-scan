626c968a50fbd13b6379ebf6c0150c025363d371
sivasakthi <itsakthi@gmail.com>
2019-04-17 18:20:22 -0500

    weekly chart in bearish and star

--- src/api/candlestick/star.js
+++ src/api/candlestick/star.js
@@ -5,15 +5,32 @@ export default class Star {
         let star = []
         let utility = new Utility()
         const reqDate = utility.formatDate(req.body.candlestickdate)
+        const range = req.body.candlestickrange
         databaseConnection.collection(collectionName).find().toArray((error, result)=> {
             if (error) return console.log(error)
             for(let index = 0;index < result.length - 1;index++) {
-                const reqDateIndex = result[index].quoteDBRecord.findIndex(dbRecord => dbRecord.quoteDate == reqDate)
-                let currentOpenPrice   = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteOpenPrice.replace(/,/g, ''))
-                let currentClosePrice  = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteClosePrice.replace(/,/g, ''))
-                let currentHighPrice   = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteHighPrice.replace(/,/g, ''))
-                let currentLowPrice    = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteLowPrice.replace(/,/g, ''))
-                
+                let reqDateIndex = 0, currentOpenPrice = 0, currentClosePrice = 0, currentHighPrice = 0, currentLowPrice = 0
+                if(range === 'daily') {
+                    reqDateIndex = result[index].quoteDBRecord.findIndex(dbRecord => dbRecord.quoteDate == reqDate)
+                    currentOpenPrice   = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteOpenPrice.replace(/,/g, ''))
+                    currentClosePrice  = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteClosePrice.replace(/,/g, ''))
+                    currentHighPrice   = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteHighPrice.replace(/,/g, ''))
+                    currentLowPrice    = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteLowPrice.replace(/,/g, ''))
+                }  else if(range === 'weekly') {
+                    let tempHigh = 0, tempLow = 0, currentDate = reqDate
+                    for(let dayCount = 1; dayCount < 6; dayCount++) {
+                        reqDateIndex = result[index].quoteDBRecord.findIndex(dbRecord => dbRecord.quoteDate == currentDate)
+                        if(reqDateIndex > 0) {
+                            if(!currentClosePrice) { currentClosePrice = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteClosePrice.replace(/,/g, '')) }
+                            tempHigh = tempHigh > parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteHighPrice.replace(/,/g, '')) ? tempHigh : parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteHighPrice.replace(/,/g, ''))
+                            currentHighPrice = tempHigh
+                            currentLowPrice = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteLowPrice.replace(/,/g, '')) < tempLow ? parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteLowPrice.replace(/,/g, '')) : tempLow
+                            tempLow = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteLowPrice.replace(/,/g, ''))
+                            currentOpenPrice = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteOpenPrice.replace(/,/g, ''))
+                        }
+                        currentDate = this.decrementDate(currentDate, 1)
+                    }
+                }
                 let isOpenEqualsClose = utility.approximateEqual(currentOpenPrice, currentClosePrice);
                 let isHighEqualsOpen = isOpenEqualsClose && utility.approximateEqual(currentOpenPrice, currentHighPrice);
                 let isLowEqualsClose = isOpenEqualsClose && utility.approximateEqual(currentClosePrice, currentLowPrice);
@@ -25,5 +42,14 @@ export default class Star {
                 star
             })
         })
+    }
+    decrementDate (currentDate, decrementBy) {
+        let utility = new Utility()
+        const formattedCurrentDate = new Date(currentDate)
+        formattedCurrentDate.setDate(formattedCurrentDate.getDate() - decrementBy)
+        let validDate = formattedCurrentDate.getFullYear() + '-'
+        validDate = validDate + (formattedCurrentDate.getMonth() < 10 ? '0' + (formattedCurrentDate.getMonth() + 1) : _formattedCurrentDate.getMonth() + 1)
+        validDate = validDate + '-' + (formattedCurrentDate.getDate() < 10 ? '0' + formattedCurrentDate.getDate() : formattedCurrentDate.getDate())
+        return utility.formatDate(validDate)
     }
 }

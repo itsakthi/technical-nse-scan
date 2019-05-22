cf1586b217225d429c775231ebb42f55cd9b6c36
sivasakthi <itsakthi@gmail.com>
2019-05-21 22:02:22 -0500

    delivery volume fix

--- dist/src/api/dv/above-previous-dv.js
+++ dist/src/api/dv/above-previous-dv.js
@@ -4,30 +4,40 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _utility = require('../utility');
+
+var _utility2 = _interopRequireDefault(_utility);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var AbovePreviousDV = function AbovePreviousDV(databaseConnection, collectionName, req, res) {
     _classCallCheck(this, AbovePreviousDV);
 
     var aboveDVPrev = req.body.above_dv_prev || 2;
+    var utility = new _utility2.default();
+    var reqDate = utility.formatDate(req.body.dvdate);
     databaseConnection.collection(collectionName).find().toArray(function (error, result) {
         if (error) return console.log(error);
         var stockName = [],
             deliveryPercentageDiff = [];
         for (var index = 0; index < result.length - 1; index++) {
-            var quoteDBRecordCount = result[index].quoteDBRecord.length;
             var prevDV = void 0,
                 currentDV = void 0;
+            var reqDateIndex = result[index].quoteDBRecord.findIndex(function (dbRecord) {
+                return dbRecord.quoteDate == reqDate;
+            });
             if (req.body.delivery === 'delivery_volume') {
-                var prevVolume = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 2].quoteVolume.replace(/,/g, ''));
-                var currentVolume = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 1].quoteVolume.replace(/,/g, ''));
+                var prevVolume = parseFloat(result[index].quoteDBRecord[reqDateIndex - 1].quoteVolume.replace(/,/g, ''));
+                var currentVolume = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteVolume.replace(/,/g, ''));
                 if (currentVolume <= prevVolume * aboveDVPrev) {
-                    prevDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 2].quoteVolumeDelivered.replace(/,/g, ''));
-                    currentDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 1].quoteVolumeDelivered.replace(/,/g, ''));
+                    prevDV = parseFloat(result[index].quoteDBRecord[reqDateIndex - 1].quoteVolumeDelivered.replace(/,/g, ''));
+                    currentDV = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteVolumeDelivered.replace(/,/g, ''));
                 }
             } else if (req.body.delivery === 'delivery_percentage') {
-                prevDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 2].quoteDeliveryPercentage.replace(/,/g, ''));
-                currentDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 1].quoteDeliveryPercentage.replace(/,/g, ''));
+                prevDV = parseFloat(result[index].quoteDBRecord[reqDateIndex - 1].quoteDeliveryPercentage.replace(/,/g, ''));
+                currentDV = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteDeliveryPercentage.replace(/,/g, ''));
             }
             if (currentDV >= prevDV * 2) {
                 stockName.push(result[index].stockCode);
--- src/api/dv/above-previous-dv.js
+++ src/api/dv/above-previous-dv.js
@@ -1,22 +1,26 @@
+import Utility from '../utility'
+
 export default class AbovePreviousDV {
     constructor(databaseConnection, collectionName, req, res) {
         const aboveDVPrev = req.body.above_dv_prev || 2
+        let utility = new Utility()
+        const reqDate = utility.formatDate(req.body.dvdate)
         databaseConnection.collection(collectionName).find().toArray((error, result)=> {
             if (error) return console.log(error)
             let stockName = [], deliveryPercentageDiff = []
             for(let index = 0;index < result.length - 1;index++) {
-                const quoteDBRecordCount = result[index].quoteDBRecord.length
                 let prevDV, currentDV
+                const reqDateIndex = result[index].quoteDBRecord.findIndex(dbRecord => dbRecord.quoteDate == reqDate)
                 if(req.body.delivery === 'delivery_volume') {
-                    const prevVolume = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 2].quoteVolume.replace(/,/g, ''))
-                    const currentVolume = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 1].quoteVolume.replace(/,/g, ''))
+                    const prevVolume = parseFloat(result[index].quoteDBRecord[reqDateIndex - 1].quoteVolume.replace(/,/g, ''))
+                    const currentVolume = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteVolume.replace(/,/g, ''))
                     if(currentVolume <= (prevVolume * aboveDVPrev)) {
-                        prevDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 2].quoteVolumeDelivered.replace(/,/g, ''))
-                        currentDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 1].quoteVolumeDelivered.replace(/,/g, ''))
+                        prevDV = parseFloat(result[index].quoteDBRecord[reqDateIndex - 1].quoteVolumeDelivered.replace(/,/g, ''))
+                        currentDV = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteVolumeDelivered.replace(/,/g, ''))
                     }
                 } else if(req.body.delivery === 'delivery_percentage') {
-                    prevDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 2].quoteDeliveryPercentage.replace(/,/g, ''))
-                    currentDV = parseFloat(result[index].quoteDBRecord[quoteDBRecordCount - 1].quoteDeliveryPercentage.replace(/,/g, ''))
+                    prevDV = parseFloat(result[index].quoteDBRecord[reqDateIndex - 1].quoteDeliveryPercentage.replace(/,/g, ''))
+                    currentDV = parseFloat(result[index].quoteDBRecord[reqDateIndex].quoteDeliveryPercentage.replace(/,/g, ''))
                 }
                 if(currentDV >= (prevDV * 2)) {
                     stockName.push(result[index].stockCode)

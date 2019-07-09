7f3df2569725c0d97557a7c7650046ac535b2cc9
sivasakthi <itsakthi@gmail.com>
2019-07-08 22:11:11 -0500

    Option price open interest change percentage

--- src/api/options/option-price.js
+++ src/api/options/option-price.js
@@ -104,8 +104,8 @@ export default class OptionPrice {
               optionDataColl.push({
                 underlyingPrice,
                 date: optionDataDate,
-                callOpenInterestChangePer: (callOpenInterestChange / (callOpenInterest - callOpenInterestChange)) * 100,
-                putOpenInterestChangePer: (putOpenInterestChange / (putOpenInterest - putOpenInterestChange)) * 100,
+                callOpenInterestChangePer: (callOpenInterestChange / (callOpenInterest - abs(callOpenInterestChange))) * 100,
+                putOpenInterestChangePer: (putOpenInterestChange / (putOpenInterest - abs(putOpenInterestChange))) * 100,
                 optionData
               })
             })
